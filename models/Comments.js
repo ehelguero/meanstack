3600035854b2cd8291130d416e9e0112767d810a
loupgarou81 <leloupgarou81@gmail.com>
2016-08-01 00:59:40 +0100

    Fix bugs with models and first routes

--- models/Comments.js
+++ models/Comments.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var CommentsSchema = new mongoose.Schema({
   body: String,
   author: {type: String, default: 'user'},
-  upvotes: {type: number, default: 0},
+  upvotes: {type: Number, default: 0},
   post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post' },
 });
 
